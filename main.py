19b9a28238a68e72ca89097842b2d3051254e0da
Afzal Siddique <afzal.siddique99@gmail.com>
2022-02-24 22:22:37 +0600

    point where the first block should start in the list

--- main.py
+++ main.py
@@ -9,14 +9,25 @@ class DrawInformation:
     RED=255,0,0
     GREY=128,128,128
     BACKGROUND_COLOR=WHITE
+
+    SIDE_PAD = 100
+    TOP_PAD = 150
     def __init__(self,width,height,lst):
         self.width=width
         self.height=height
 
         self.window = pygame.display.set_mode((width,height))
         pygame.display.set_caption("Sorting Algorithm Visualizer")
-        # self.set_list(lst)
+        self.set_list(lst)
 
-DrawInformation(200,500,[])
 
-    # def set_list(self, lst):
+    def set_list(self, lst):
+        self.lst=lst
+        self.min_val = min(lst)
+        self.max_val = max(lst)
+
+        self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
+        self.block_height = round((self.height-self.TOP_PAD)/(self.max_val-self.min_val))
+        self.start_x = self.SIDE_PAD // 2
+
+# DrawInformation(200,500,[])
