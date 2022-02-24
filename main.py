8b900e34df5eefea8d804d26a1c531835f5dfe74
Afzal Siddique <afzal.siddique99@gmail.com>
2022-02-24 22:53:42 +0600

    draw list on pygame window

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ class DrawInformation:
     WHITE=255,255,255
     GREEN=0,255,0
     RED=255,0,0
-    GREY=128,128,128
     BACKGROUND_COLOR=WHITE
+
+    GRADIENTS = [(128,128,128), (160,160,160),(192,192,192)]
 
     SIDE_PAD = 100
     TOP_PAD = 150
@@ -29,6 +30,23 @@ class DrawInformation:
         self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
         self.block_height = round((self.height-self.TOP_PAD)/(self.max_val-self.min_val))
         self.start_x = self.SIDE_PAD // 2
+def draw(draw_info):
+    draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+    draw_list(draw_info)
+    pygame.display.update()
+
+def draw_list(draw_info:DrawInformation):
+    lst = draw_info.lst
+    for i, val in enumerate(lst):
+        x = draw_info.start_x + i * draw_info.block_width
+        y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+
+        color = draw_info.GRADIENTS[i % 3]
+
+
+        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.height))
+
+
 
 def generate_starting_list(n,min_val,max_val):
     lst = []
@@ -49,6 +67,7 @@ def main():
     draw_info=DrawInformation(800,600,lst)
     while run:
         clock.tick(60)
+        draw(draw_info)
         pygame.display.update()
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
